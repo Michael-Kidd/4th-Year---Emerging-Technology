fa6ea2279474a0e48a58c00e245d341d7ae339ea
Michael Kidd <G00236920@gmit.ie>
2018-11-21 22:10:39 +0000

    Added a drawable canvas in the project using tkinter

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -1,22 +1,43 @@
-import gzip
-import numpy as np
-import matplotlib.pyplot as plt
+import tkinter as tk
+# Tkinter is Python's de-facto standard GUI (Graphical User Interface) package.
 
-# Unzip contents and read the file
-with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
-    file_content = f.read()
+def clearCoords(event):
+    # when left click released, remove the old coordinates so you can draw multiple non conjoined lines
+    # Without this, if you draw a seven or some letters, it would draw a continuous line, unbroken
+    canvas.old_coords = None
 
-# get the bytes from the file
-file_content[0:4]
+def clearCanvas(event):
+    canvas.old_coords = None
+    canvas.delete("all")
 
-# read one image as a byte array
-l = file_content[16:800]
+def draw(event):
 
-image = ~np.array(list(file_content[16:800])).reshape(28,28).astype(np.uint8)
+    # set X and Y coordinates to the event X and Y.
+    x, y = event.x, event.y
 
-plt.imshow(image, cmap='gray')
+    # if the old coordinates have a value
+    if canvas.old_coords:
+        # set x1 and y1 coordinates to the old coordinates
+        x1, y1 = canvas.old_coords
+        # create a line, from the new coordinates, back to the old coordinates
+        canvas.create_line(x, y, x1, y1)
+    # set the old coordinates to the values that the new coordinates to be used as old coordinates, next iteration    
+    canvas.old_coords = x, y
 
-with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
-    labels = f.read()
+root = tk.Tk()
 
-int.from_bytes(labels[8:9], byteorder="big")
+# show a canvas page
+canvas = tk.Canvas(root, width=400, height=400)
+# pack the canvas to be show
+canvas.pack()
+# set the old coords as null
+canvas.old_coords = None
+
+# If the left mouse button held, Run the function that will draw a line
+root.bind('<B1-Motion>', draw)
+# If the right mouse button pressed, Run the function that clear the canvas
+root.bind('<Button-3>', clearCanvas)
+# When 
+root.bind('<ButtonRelease-1>', clearCoords)
+# loop 
+root.mainloop()
