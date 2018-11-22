1b16ce93506adcf0bbc1c6b66b5fe20dab8df5ed
Michael Kidd <G00236920@gmit.ie>
2018-11-22 17:13:48 +0000

    Attempting different approaches to training the nueral net

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -1,12 +1,60 @@
-import tkinter as tk
 # Tkinter is Python's de-facto standard GUI (Graphical User Interface) package.
+import tkinter as tk
+import keras as kr
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import tensorflow as tf
+# For encoding categorical variables.
+import sklearn.preprocessing as pre
+
+import gzip
+
+def nueralNet(image):
+
+    # Read in images for training
+    with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+        train_img = f.read()
+
+    # read in labels for training
+    with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+        train_lbl = f.read()
+
+    # Read in test images
+    with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+        test_img = f.read()
+
+    # read in test labels
+    with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+        test_lbl = f.read()
+
+    # Start a neural network, building it by layers.
+    # using sequential model
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(train_img, train_lbl, epochs=5)
+    model.evaluate(test_img, test_lbl)
+
 
 def check():
-    # Button click and user intends to check a Digit from a Drawing
-    print('test')
+    # Button clicked and user intends to check a Digit from a Drawing
+    canvas.update()
+    canvas.postscript(file="image.ps", colormode='gray')
+
+    with open("image.ps", "rb") as imageFile:
+        file_content = imageFile.read()
+
+    nueralNet(file_content)
 
 def checkFile():
-    # Button click and user intends to check a Digit from a File
+    # Button clicked and user intends to check a Digit from a File
     print('test2')
 
 def clearCoords(event):
@@ -21,10 +69,8 @@ def clearCanvas(event):
     canvas.delete("all")
 
 def draw(event):
-
     # set X and Y coordinates to the event X and Y.
     x, y = event.x, event.y
-
     # if the old coordinates have a value
     if canvas.old_coords:
         # set x1 and y1 coordinates to the old coordinates
@@ -34,14 +80,13 @@ def draw(event):
     # set the old coordinates to the values that the new coordinates to be used as old coordinates, next iteration    
     canvas.old_coords = x, y
 
-
 # Main Window - canvas
 root = tk.Tk()
 # second window - control panel
 second_win = tk.Toplevel(root)
 
 # show a canvas page
-canvas = tk.Canvas(root, width=400, height=400)
+canvas = tk.Canvas(root, width=560, height=560)
 canvas2 = tk.Canvas(second_win, width=200, height=300)
 
 # create text
@@ -53,15 +98,15 @@ canvas.pack()
 canvas2.pack()
 
 # Tkinter Button
-B = tk.Button(second_win, text ="Check File", command = check)
+B = tk.Button(second_win, text ="Check File", command = checkFile)
 B.pack()
 
-B2 = tk.Button(second_win, text ="Check Drawing", command = checkFile)
+B2 = tk.Button(second_win, text ="Check Drawing", command = check)
 B2.pack()
 
 # set the position of the windows
 root.geometry("+{xPos}+{yPos}".format(xPos = 0, yPos = 0))
-second_win.geometry("+{xPos}+{yPos}".format(xPos = 410, yPos = 0))
+second_win.geometry("+{xPos}+{yPos}".format(xPos = 561, yPos = 0))
 
 # set the old coords as null
 canvas.old_coords = None
