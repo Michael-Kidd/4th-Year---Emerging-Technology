c4d450550d799d250c19a9dbe6eb61acb1386720
Michael Kidd <G00236920@gmit.ie>
2018-11-27 12:51:49 +0000

    EDITED COMMENTS

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import math
 import sklearn.preprocessing as pre
 import gzip
-
-from PIL import ImageTk, Image, ImageDraw, ImageFilter
 import PIL
+from PIL import Image, ImageDraw
+
 
 width = 280
 height = 280
@@ -19,6 +19,7 @@ black = (0,0,0)
 
 def nueralNet(img):
 
+    # global variables - in place of static variables
     global model, result
 
     # Read in images for training
@@ -41,7 +42,7 @@ def nueralNet(img):
     model.add(kr.layers.Dense(10, activation="softmax", kernel_initializer="normal"))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    # reshape the images and labels into 28x28 arrays.
+    # reshape the images and labels.
     train_img = ~np.array(list(train_img[16:])).reshape(60000, 1, 784).astype(np.uint8)
     train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
 
@@ -58,7 +59,7 @@ def nueralNet(img):
     outputs = encoder.transform(train_lbl)
 
     # Train the model
-    model.fit(inputs, outputs, epochs=5, batch_size=1000)
+    model.fit(inputs, outputs, epochs=5, batch_size=100)
 
     test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
     test_lbl =  np.array(list(test_lbl[ 8:])).astype(np.uint8)
@@ -74,10 +75,12 @@ def nueralNet(img):
 
 
 def clearCanvas(event):
-    
+    # global variables
     global image1, draw
 
+    # clears the canvas seen by the user
     cv.delete("all")
+    # clear the pillow image that is not seen by the user
     image1 = PIL.Image.new("RGB", (width, height), black)
     draw = ImageDraw.Draw(image1)
     
@@ -86,36 +89,51 @@ def save():
 
     global image1
 
+    # resize the image so it matches the mnist data set conditions
     img = image1.resize((28, 28), Image.BICUBIC)
+    # save the image
     img.save("image.png")
-    
+    # read back in the image,
+    # I chose to do it this way in case i wanted to give it an image
+    # or have the user do it
     img = imageprepare('image.png')
-
+    # start the nueral network
     nueralNet(img)
     
 
 def paint(event):
+    # creates a line using mouse events
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
+
+    # create an dot using these positions 
+    # pillow image - not seen by user
     cv.create_oval(x1, y1, x2, y2, fill="black",width=12)
+    # canvas image - seen by user
     draw.line([x1, y1, x2, y2],fill="white",width=12)
 
 def imageprepare(argv):
 
+    # read in an image and greyscale
     im = Image.open(argv).convert('L')
+    
+    # uncomment to view the incoming image 
     # im.show()
 
+    # get the data from the image
     tv = list(im.getdata())
 
     # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
     tv = [(255 - x) * 1.0 / 255.0 for x in tv]
     
+    # return the image data
     return tv
 
 # Start a neural network, building it by layers.
 # using sequential model
 model = kr.models.Sequential()
 
+# new pillow image that later will be saved
 image1 = PIL.Image.new("RGB", (width, height), black)
 draw = ImageDraw.Draw(image1)
 
@@ -128,7 +146,9 @@ cv = tk.Canvas(root, width=width, height=height, bg='white')
 
 # pack the gui
 cv.pack()
+# left click
 cv.bind("<B1-Motion>", paint)
+# right click
 cv.bind('<Button-3>', clearCanvas)
 
 # create text and buttons
@@ -137,6 +157,7 @@ text1 = tk.Label(text="Left Click Draw")
 text2 = tk.Label(text="Right Click Clear")
 result = tk.Label(text="You have not Checked a Number yet")
 
+# pack the canvas
 text1.pack()
 text2.pack()
 button.pack()
